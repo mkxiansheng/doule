717c3f5c28cbf447060f639ede97dca0a49d95f9
mk <17trackNET>
2016-10-20 17:36:47 +0800

    updata

--- pages/my/my.js
+++ pages/my/my.js
@@ -3,24 +3,11 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    name: 'mk先生',
+    pic_url: '../../images/my-code.png',
+    github: 'https://github.com/mkxiansheng/doule'
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+
   }
 })
