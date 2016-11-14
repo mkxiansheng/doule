c8322b766e483015cf664d48f0fc73ab6561e343
mk <17trackNET>
2016-11-14 18:46:37 +0800

    updata

--- pages/collection/collection.js
+++ pages/collection/collection.js
@@ -4,14 +4,7 @@ var app = getApp()
 Page({
   data: {
     dec: "暂无收藏!",
-    req: [{
-    	"content":"女生分手的原因有两个， 一个是：闺蜜看不上。另一个是：闺蜜看上了。",
-    	"hashId":"607ce18b4bed0d7b0012b66ed201fb08",
-    	"unixtime":1418815439,
-    	"updatetime":"2014-12-17 19:23:59",
-    	"zan": 99,
-    	"cai": 22
-    }]
+    req: []
   },
   onLoad () {
 
