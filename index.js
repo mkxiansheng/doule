b03b939b9dd341a54b4a9ca1cbbcaf4eec9440fe
mk <17trackNET>
2016-11-28 16:44:29 +0800

    add

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ var mongodb = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/doule');
 
+var fs = require('fs');
+var multer  = require('multer')
+
+app.use('/',express.static(__dirname));
+
+// 获取pic
 app.get('/api/pic', function(req,res){
 	var dbs = db;
 	var collection = dbs.get('pic');
@@ -22,6 +28,7 @@ app.get('/api/pic', function(req,res){
 	});
 });
 
+// 获取joke
 app.get('/api/joke', function(req,res){
 	var dbs = db;
 	var collection = dbs.get('joke');
@@ -38,6 +45,7 @@ app.get('/api/joke', function(req,res){
 	});
 });
 
+// 获取收藏
 app.get('/api/collection', function(req,res){
 	var dbs = db;
 	var collection = dbs.get('collection');
@@ -54,6 +62,7 @@ app.get('/api/collection', function(req,res){
 	});
 });
 
+// 添加pic
 app.get('/api/updata/pic', function(req,res){
 	var dbs = db;
 	var collection = dbs.get('pic');
@@ -78,17 +87,13 @@ app.get('/api/updata/pic', function(req,res){
     })
 });
 
+// 添加joke
 app.get('/api/updata/joke', function(req,res){
+	console.log(req.query)
+	var newInfo = req.query;
 	var dbs = db;
 	var collection = dbs.get('joke');
-	collection.insert({
-      "content": '女票一直是个女汉子，为了让我在朋友面前有面子，展现她阴柔娇弱的一面，吃夜宵摊时她说：“老公~帮人家拧开这个啤酒瓶盖嘛~”吓得我坐到地上！',
-      "hashId":"607ce06b4bed0d7b0012b66ed201fb02",
-      "unixtime":1418815449,
-      "updatetime":"2014-12-19 10:23:59",
-      "zan": 11,
-      "cai": 2
-    },function(e,docs){
+	collection.insert(newInfo,function(e,docs){
     	if (e) {
     		console.log(e)
     		var err = {
@@ -101,6 +106,7 @@ app.get('/api/updata/joke', function(req,res){
     })
 });
 
+// 添加收藏
 app.get('/api/updata/collection', function(req,res){
 	var dbs = db;
 	var collection = dbs.get('collection');
@@ -124,6 +130,7 @@ app.get('/api/updata/collection', function(req,res){
     })
 });
 
+// 取消收藏
 app.get('/api/del/collection', function(req,res){
 	var dbs = db;
 	var collection = dbs.get('collection');
@@ -140,6 +147,34 @@ app.get('/api/del/collection', function(req,res){
     		res.end(JSON.stringify(docs))
     	}
     })
+});
+
+//上传图片
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/img')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.' + (file.originalname).split(".")[1])
+  }
+})
+
+var upload = multer({ storage: storage })
+
+
+// 单图上传
+app.post('/upload', upload.single('logo'), function(req, res, next){
+    var file = req.file;
+
+    console.log('文件类型：%s', file.mimetype);
+    console.log('原始文件名：%s', file.originalname);
+    console.log('文件大小：%s', file.size);
+    console.log('文件保存路径：%s', file.path);
+    var data = {
+      imgUrl : file.path
+    }
+    console.log(data.imgUrl);
+    res.end(JSON.stringify(data));
 });
 
 app.listen(4466)
--- pages/add/add.js
+++ pages/add/add.js
@@ -1,43 +1,120 @@
 //index.js
 //获取应用实例
-var app = getApp()
+const app = getApp()
+
+const API_URL_JOKE = 'http://localhost:4466/api/updata/joke'
+const API_URL_PIC = 'http://localhost:4466/api/updata/pic'
+const API_URL_IMG = 'http://localhost:4466/upload'
+
 Page({
 
   data: {
-  	newJoke: ''
+  	newJoke: '',
+    ispic: false,
+    pic: ''
   },
 
   addNew: function(){
   	var that = this
   	console.log('in click')
-  	app.addReq(that.data.newJoke,'add')
+
+    var chars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    var hashId = ''
+    for(var i=0,len=32;i<len;i++){
+        var key = Math.ceil(Math.random()*35)
+        hashId += chars[key]
+    }
+
+    var time = new Date()
+    var updatetime = time.toLocaleString()
+    var newInfo;
+
+    var t = '';
+    t = setTimeout(function(){
+
+      if (that.data.pic) {
+        newInfo = {
+          "content": that.data.newJoke,
+          "hashId": hashId,
+          "updatetime": updatetime,
+          "url": that.data.pic    
+        }   
+        wx.request({
+          url: API_URL_PIC,
+          data: newInfo,
+          header: { 'Content-Type': 'application/json' },
+          success (res) {
+            // callback(null, res.data)
+            console.log(res.data)
+          },
+          fail (e) {
+            callback(e)
+          }
+        })       
+      }else{
+        newInfo = {
+          "content": that.data.newJoke,
+          "hashId": hashId,
+          "updatetime": updatetime
+        }      
+        wx.request({
+          url: API_URL_JOKE,
+          data: newInfo,
+          header: { 'Content-Type': 'application/json' },
+          success (res) {
+            // callback(null, res.data)
+            console.log(res.data)
+          },
+          fail (e) {
+            callback(e)
+          }
+        })
+      }
+      
+    }, 300)
+
+
   },
 
   textContent: function(e){
 
   	var that = this
 
-  	var chars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  	var hashId = ''
-  	for(var i=0,len=32;i<len;i++){
-        var key = Math.ceil(Math.random()*35)
-        hashId += chars[key]
-  	}
-
-  	var content = e.detail.value
-
-  	var time = new Date()
-  	var updatetime = time.toLocaleString()
-  	var newJoke = {
-    	"content": content,
-    	"hashId": hashId,
-    	"updatetime": updatetime		
-  	}
+    var content = e.detail.value
 
     that.setData({
-    	newJoke: newJoke
+    	newJoke: content
     })
 
+  },
+
+  addPic: function(e){
+    var that = this;
+    console.log(that);
+    wx.chooseImage({
+      success: function(res) {
+        var tempFilePaths = res.tempFilePaths
+        wx.uploadFile({
+          url: API_URL_IMG, 
+          header: {
+            'content-type': 'multipart/form-data'
+          },
+          filePath: tempFilePaths[0],
+          name: 'logo',
+          success: function(res){
+            var data = res.data
+            // var datas = JSON.parse(data);
+            // console.log(datas.imgUrl);
+            var picUrl = 'http://localhost:4466/'+data
+            that.setData({
+              ispic: true,
+              pic : picUrl
+            })
+            console.log(that.data.pic);
+          }
+        })
+      }
+    })    
   },
 
   onLoad: function () {
